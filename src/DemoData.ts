9eafc69d82dce4d2c558e9b0e9c693a83887ac71
WaleyChAn <zerogeneral@foxmail.com>
2024-06-21 00:55:23 +0800

    feat: 修改布局模式

---
+++ src/DemoData.ts
@@ -0,0 +1,47 @@
+const demoData = {
+  "nodes":
+    [
+      { "id": "1", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "2", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "3", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "4", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "5", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "6", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "7", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "8", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "9", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "10", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "11", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "12", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "13", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "14", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "15", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "16", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "17", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "18", "type": "selectorNode", "position": { "x": 0, "y": 0 } },
+      { "id": "19", "type": "selectorNode", "position": { "x": 0, "y": 0 } }
+    ],
+  "edges": [
+    // { "id": "0-1", "source": "undefined", "target": "1", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "1-2", "source": "1", "target": "2", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "1-3", "source": "1", "target": "3", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "1-4", "source": "1", "target": "4", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "1-5", "source": "1", "target": "5", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "2-6", "source": "2", "target": "6", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "2-7", "source": "2", "target": "7", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "2-8", "source": "2", "target": "8", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "3-9", "source": "3", "target": "9", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "3-10", "source": "3", "target": "10", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "3-11", "source": "3", "target": "11", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "3-12", "source": "3", "target": "12", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "4-13", "source": "4", "target": "13", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "4-14", "source": "4", "target": "14", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "5-15", "source": "5", "target": "15", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "6-16", "source": "6", "target": "16", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "6-17", "source": "6", "target": "17", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "8-18", "source": "8", "target": "18", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } },
+    { "id": "9-19", "source": "9", "target": "19", "type": "custom", "animated": false, "label": "", "style": { "strokeWidth": 3 }, "markerEnd": { "type": "arrowclosed" } }
+  ]
+}
+
+export { demoData }
---
+++ src/DemoData2.ts
@@ -0,0 +1,999 @@
+const demoData = {
+  "nodes": [
+    {
+      "id": "1",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "2",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "3",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "4",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "5",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "6",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "7",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "8",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "9",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "10",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "11",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "12",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "13",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "14",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "15",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "16",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "17",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "18",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "19",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "20",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "21",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "22",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "23",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "24",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "25",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "26",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "27",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "28",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "29",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "30",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "31",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "32",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "33",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "34",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "35",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "36",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "37",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "38",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "39",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "40",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "41",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "42",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "43",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "44",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    },
+    {
+      "id": "45",
+      "type": "selectorNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    }
+  ],
+  "edges": [
+    // {
+    //   "id": "0-1",
+    //   "source": "undefined",
+    //   "target": "1",
+    //   "type": "custom",
+    //   "animated": false,
+    //   "label": "",
+    //   "style": {
+    //     "strokeWidth": 3
+    //   },
+    //   "markerEnd": {
+    //     "type": "arrowclosed"
+    //   }
+    // },
+    {
+      "id": "1-2",
+      "source": "1",
+      "target": "2",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "1-3",
+      "source": "1",
+      "target": "3",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "1-4",
+      "source": "1",
+      "target": "4",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "1-5",
+      "source": "1",
+      "target": "5",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "2-6",
+      "source": "2",
+      "target": "6",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "2-7",
+      "source": "2",
+      "target": "7",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "2-8",
+      "source": "2",
+      "target": "8",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "3-9",
+      "source": "3",
+      "target": "9",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "3-10",
+      "source": "3",
+      "target": "10",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "3-11",
+      "source": "3",
+      "target": "11",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "3-12",
+      "source": "3",
+      "target": "12",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "4-13",
+      "source": "4",
+      "target": "13",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "4-14",
+      "source": "4",
+      "target": "14",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "5-14",
+      "source": "5",
+      "target": "15",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "6-16",
+      "source": "6",
+      "target": "16",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "6-17",
+      "source": "6",
+      "target": "17",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "8-18",
+      "source": "8",
+      "target": "18",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "9-19",
+      "source": "9",
+      "target": "19",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "9-20",
+      "source": "9",
+      "target": "20",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "9-21",
+      "source": "9",
+      "target": "21",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "11-22",
+      "source": "11",
+      "target": "22",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "11-23",
+      "source": "11",
+      "target": "23",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "11-24",
+      "source": "11",
+      "target": "24",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "11-25",
+      "source": "11",
+      "target": "25",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "24-26",
+      "source": "24",
+      "target": "26",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "24-27",
+      "source": "24",
+      "target": "27",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "6-28",
+      "source": "6",
+      "target": "28",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "6-29",
+      "source": "6",
+      "target": "29",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "29-30",
+      "source": "29",
+      "target": "30",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "29-31",
+      "source": "29",
+      "target": "31",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "31-32",
+      "source": "31",
+      "target": "32",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "31-33",
+      "source": "31",
+      "target": "33",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "31-34",
+      "source": "31",
+      "target": "34",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "15-35",
+      "source": "15",
+      "target": "35",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "15-36",
+      "source": "15",
+      "target": "36",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "14-37",
+      "source": "14",
+      "target": "37",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "23-38",
+      "source": "23",
+      "target": "38",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "23-39",
+      "source": "23",
+      "target": "39",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "23-40",
+      "source": "23",
+      "target": "40",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "23-41",
+      "source": "23",
+      "target": "41",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "23-42",
+      "source": "23",
+      "target": "42",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "23-43",
+      "source": "23",
+      "target": "43",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "23-44",
+      "source": "23",
+      "target": "44",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    },
+    {
+      "id": "23-45",
+      "source": "23",
+      "target": "45",
+      "type": "custom",
+      "animated": false,
+      "label": "",
+      "style": {
+        "strokeWidth": 3
+      },
+      "markerEnd": {
+        "type": "arrowclosed"
+      }
+    }
+  ]
+}
+
+
+export { demoData }
